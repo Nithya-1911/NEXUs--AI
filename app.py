ebc0f6496299e2801f9125f21bb4060fb56f8221
Nithya <vallampatlanithya@gmail.com>
2026-08-08 20:42:08 +0530

    Prepare Flask app for deployment

--- app.py
+++ app.py
@@ -594,8 +594,5 @@ def home():
 # -----------------------------
 
 if __name__ == "__main__":
-
-    app.run(
-        debug=True,
-        use_reloader=False
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
